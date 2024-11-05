21903cfd06fc0a820d694b957219ae154c215d48
Bilal Khan <bilal.khan@myskysys.com>
2024-11-05 17:36:43 +0500

    sign up and sign in pages

--- app/components/ClientOnly/ClientOnly.tsx
+++ app/components/ClientOnly/ClientOnly.tsx
@@ -8,22 +8,26 @@ interface ClientOnlyProps {
   children: React.ReactNode;
 }
 
-const ClientOnly: React.FC<ClientOnlyProps> = ({
-  children
-}) => {
+const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
   const [hasMounted, setHasMounted] = useState(false);
+  const [isAuthRoute, setIsAuthRoute] = useState(false);
 
   useEffect(() => {
     setHasMounted(true);
-  }, [])
+
+    // Check if the current path starts with "Auth"
+    if (typeof window !== 'undefined') {
+      setIsAuthRoute(window.location.pathname.startsWith('/Auth'));
+    }
+  }, []);
 
   if (!hasMounted) return null;
 
   return (
     <>
-    <Navbar/>
-      { children }
-    <Footer/>
+      {!isAuthRoute && <Navbar />}
+      {children}
+      {!isAuthRoute && <Footer />}
     </>
   );
 };
