268fffa6353eb18915724bfaafc6986a8376cd0c
khizerjilaniskysys <khizer.jilani@myskysys.com>
2024-11-18 11:51:45 +0500

    Rename env keys

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -4,8 +4,8 @@ import { uploadSchema } from '@/Validation/Server/validator';
 
 // AWS S3 configuration
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_KEY,
+  secretAccessKey: process.env.AWS_SECRET_KEY_KEY,
   region: process.env.AWS_REGION,
 });
 
